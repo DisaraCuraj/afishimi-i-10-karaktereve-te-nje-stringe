1d582cb4bb2e5be0d1249114893e21c85863cea2
disar_000 <disaracuraj@hotmail.com>
2020-06-01 18:25:14 +0100

    Afishim i 10 karaktereve te nje stringe

---
+++ Afishim i 10 karaktereve te nje stringe/src/afishi_i_10_karaktereve_te_nje_stringe.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class afishi_i_10_karaktereve_te_nje_stringe {
+
+	public static void main(String[] args) {
+		Scanner input=new Scanner(System.in);
+		String d="qwarteu123456";
+		System.out.printf("%.10s %n",d);
+		
+
+	}
+
+}
